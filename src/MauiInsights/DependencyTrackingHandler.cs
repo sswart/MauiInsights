2dfc3a0a9757c80dd0cd45853930f7e91af836a1
Jesse Swart <jswart@xpirit.com>
2022-05-02 13:11:45 +0200

    Maui insights

---
+++ src/MauiInsights/DependencyTrackingHandler.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.Http;
+
+namespace MauiInsights
+{
+    public class DependencyTrackingHandler : DelegatingHandler
+    {
+        private readonly TelemetryClient _client;
+
+        public DependencyTrackingHandler(TelemetryClient client)
+        {
+            _client = client;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var telemetry = GetTelemetry(request);
+            SetOpenTelemetryHeaders(request, telemetry);
+            telemetry.Start();
+            HttpResponseMessage? response = null;
+            try
+            {
+                response = await base.SendAsync(request, cancellationToken);
+                return response;
+            }
+            finally
+            {
+                telemetry.Stop();
+                Enrich(telemetry, response);
+                _client.TrackDependency(telemetry);
+            }
+        }
+
+        private static DependencyTelemetry GetTelemetry(HttpRequestMessage request)
+        {
+            var host = request.RequestUri.Host;
+            var call = request.RequestUri.AbsolutePath;
+            var operationId = Guid.NewGuid().ToString().Replace("-", "");
+            var telemetry = new DependencyTelemetry("Http", host, call, "");
+            telemetry.Context.Operation.Id = operationId;
+            return telemetry;
+        }
+
+        private void Enrich(DependencyTelemetry telemetry, HttpResponseMessage? response)
+        {
+            telemetry.Success = response != null;
+            telemetry.ResultCode = response?.StatusCode.ToString();
+        }
+
+        private void SetOpenTelemetryHeaders(HttpRequestMessage request, DependencyTelemetry telemetry)
+        {
+            var parentId = telemetry.Context.Operation.Id;
+            var traceId = telemetry.Id;
+            var version = "00";
+            var flags = _client.Context.Flags;
+
+            var headerValue = $"{version}-{parentId}-{traceId}-{flags}";
+            request.Headers.Add("traceparent", headerValue);
+        }
+    }
+
+    public class DependencyTrackingHandlerFilter : IHttpMessageHandlerBuilderFilter
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public DependencyTrackingHandlerFilter(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next)
+        {
+            return (builder) =>
+            {
+                next(builder);
+
+                if (_serviceProvider.GetService(typeof(DependencyTrackingHandler)) is DependencyTrackingHandler handler)
+                {
+                    builder.AdditionalHandlers.Add(handler);
+                }
+            };
+        }
+    }
+}
---
+++ src/MauiInsights/MauiAppBuilderExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Options;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Hosting;
+
+namespace MauiInsights;
+
+public static class MauiAppBuilderExtensions
+{
+    private static TelemetryClient? _client;
+    public static MauiAppBuilder AddApplicationInsights(this MauiAppBuilder appBuilder, string appInsightsConnectionString)
+    {
+        SetupTelemetryClient(appBuilder, appInsightsConnectionString);
+        SetupHttpDependecyTracking(appBuilder);
+        SetupPageViewTelemetry();
+        SetupLogger(appBuilder, appInsightsConnectionString);
+        return appBuilder;
+    }
+
+    private static void SetupTelemetryClient(MauiAppBuilder appBuilder, string appInsightsConnectionString)
+    {
+        var configuration = new TelemetryConfiguration()
+        {
+            ConnectionString = appInsightsConnectionString,
+        };
+        _client = new TelemetryClient(configuration);
+        appBuilder.Services.AddSingleton(_client);
+    }
+
+    private static void SetupHttpDependecyTracking(MauiAppBuilder appBuilder)
+    {
+        appBuilder.Services.AddTransient<DependencyTrackingHandler>();
+        appBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IHttpMessageHandlerBuilderFilter, DependencyTrackingHandlerFilter>());
+    }
+
+    private static void SetupPageViewTelemetry()
+    {
+        Microsoft.Maui.Handlers.PageHandler.Mapper.AppendToMapping(nameof(IContentView.Content), (handler, view) =>
+        {
+            if (view is Page)
+            {
+                _client.TrackPageView(view.GetType().Name);
+            }
+        });
+    }
+
+    private static void SetupLogger(MauiAppBuilder appBuilder, string appInsightsConnectionString)
+    {
+        var telemetryConfig = new TelemetryConfiguration()
+        {
+            ConnectionString = appInsightsConnectionString
+        };
+        var logConfig = new ApplicationInsightsLoggerOptions()
+        {
+            FlushOnDispose = true,
+            TrackExceptionsAsExceptionTelemetry = true
+        };
+        appBuilder.Logging.AddProvider(
+            new ApplicationInsightsLoggerProvider(
+                Options.Create(telemetryConfig), Options.Create(logConfig)));
+    }
+}
